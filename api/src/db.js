6444c4438b1447a183845124776626e81a3897bb
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-09-12 22:17:32 -0300

    db_postgres

--- api/src/db.js
+++ api/src/db.js
@@ -2,17 +2,14 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+const { DB_POSTGRES } = process.env;
 // const Genres= require("./models/Genres")
 // const Videogames = require("./models/Videogames")
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+const sequelize = new Sequelize(`${DB_POSTGRES}`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
